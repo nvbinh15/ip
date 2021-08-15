9621d915612aaed85ebf7ded331ffd1ac3b7353d
nvbinh15 <binh.pdc01@gmail.com>
2021-08-15 15:51:20 +0800

    Add level-0 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontal_line = "__________________________________________________";
+        System.out.println(horizontal_line);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontal_line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontal_line);
     }
 }
