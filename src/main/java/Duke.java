aa481d58f5fafc16ff53d5160c6cd217f3bfc88b
nvbinh15 <binh.pdc01@gmail.com>
2021-08-24 00:23:54 +0800

    Add Greet, Echo, and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String horizontal_line = "__________________________________________________";
-        System.out.println(horizontal_line);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println(horizontal_line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontal_line);
+        final String HORIZONTAL_LINE = "__________________________________________________";
+        System.out.println("\t" + HORIZONTAL_LINE);
+        System.out.println("\t" + "Hello! I'm Duke");
+        System.out.println("\t" + "What can I do for you?");
+        System.out.println("\t" + HORIZONTAL_LINE);
+
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+
+        while (!line.equals("bye")) {
+            System.out.println("\t" + HORIZONTAL_LINE);
+            System.out.println("\t" + line);
+            System.out.println("\t" + HORIZONTAL_LINE);
+            line = in.nextLine();
+        }
+
+        System.out.println("\t" + HORIZONTAL_LINE);
+        System.out.println("\t" + "Bye. Hope to see you again soon!");
+        System.out.println("\t" + HORIZONTAL_LINE);
     }
 }
