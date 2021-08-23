74784bd9a58b5e1484771a22867f4979affd614d
nvbinh15 <binh.pdc01@gmail.com>
2021-08-24 00:47:43 +0800

    Add storing text entered and displaying them when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,12 +9,23 @@ public class Duke {
         System.out.println("\t" + "What can I do for you?");
         System.out.println("\t" + HORIZONTAL_LINE);
 
+        String[] tasks = new String[100];
+        int numberOfTasks = 0;
+
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
 
         while (!line.equals("bye")) {
             System.out.println("\t" + HORIZONTAL_LINE);
-            System.out.println("\t" + line);
+            if (line.equals("list")) {
+                for (int i = 0; i < numberOfTasks; i++) {
+                    System.out.println("\t" + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[numberOfTasks] = line;
+                numberOfTasks++;
+                System.out.println("\t" + "added: " + line);
+            }
             System.out.println("\t" + HORIZONTAL_LINE);
             line = in.nextLine();
         }
